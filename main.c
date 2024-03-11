b62dbbc1eb3a4407693b06ac7aa15dbb7f30cd17
_N3m0 <n3m0o7@hotmail.com>
2024-03-11 16:43:17 +0100

    grid

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 static inline int min(int a, int b) {
     return a < b ? a : b;
 }
+static inline int max(int a, int b) {
+    return a > b ? a : b;
+}
 
 int screen_width = 500;
 int screen_height = 550;
@@ -13,6 +16,10 @@ int nb_cell = 16;
 
 Rectangle grid = {0};
 
+Image tile_image_orig = {0};
+Texture tile_texture = {0};
+Image tile_hover_image_orig = {0};
+Texture tile_hover_texture = {0};
 Image mine_image_orig = {0};
 Texture mine_texture = {0};
 
@@ -22,9 +29,19 @@ void screen_resize_handle(void) {
 
     menu.width = screen_width;
     grid_len = min(screen_width, screen_height - menu.height);
-    grid = (Rectangle){(screen_width - grid_len) / 2,
-                        screen_height - grid_len,
-                        grid_len, grid_len};
+    grid = (Rectangle){
+        (screen_width - grid_len) / 2,
+        screen_height - grid_len,
+        grid_len, grid_len
+    };
+
+    Image tile_copy = ImageCopy(tile_image_orig);
+    ImageResize(&tile_copy, grid.width/nb_cell, grid.height/nb_cell);
+    tile_texture = LoadTextureFromImage(tile_copy);
+
+    Image tile_hover_copy = ImageCopy(tile_hover_image_orig);
+    ImageResize(&tile_hover_copy, grid.width/nb_cell, grid.height/nb_cell);
+    tile_hover_texture = LoadTextureFromImage(tile_hover_copy);
 
     Image mine_copy = ImageCopy(mine_image_orig);
     ImageResize(&mine_copy, grid.width/nb_cell, grid.height/nb_cell);
@@ -35,9 +52,12 @@ int main(void) {
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 
     menu = (Rectangle){0, 0, screen_width, screen_height - screen_width};
+    tile_image_orig = LoadImage("ressources/tile.png");
+    tile_hover_image_orig = LoadImage("ressources/tile_hover.png");
     mine_image_orig = LoadImage("ressources/mine.png");
 
     InitWindow(screen_width, screen_height, "mineur");
+    SetTargetFPS(60);
 
     screen_resize_handle();
 
@@ -47,18 +67,41 @@ int main(void) {
 
         BeginDrawing();
         {
-            ClearBackground(RAYWHITE);
+            ClearBackground(BLACK);
 
             DrawRectangleRec(menu, RED);
-            DrawRectangleRec(grid, GRAY);
+            DrawRectangleRec(grid, (Color) {
+                .r = 0xFF, .g = 0xDA, .b = 0x2E, .a = 255
+            });
 
             for (int x=0; x<nb_cell; x++) {
                 for (int y=0; y<nb_cell; y++) {
-                    DrawTexture(mine_texture,
-                                grid.x + grid.width/nb_cell * x,
-                                grid.y + grid.height/nb_cell * y,
-                                WHITE);
+                    DrawTexture(
+                        tile_texture,
+                        grid.x + grid.width/nb_cell * x,
+                        grid.y + grid.height/nb_cell * y,
+                        WHITE
+                    );
                 }
+            }
+
+            int mouse_x = GetMouseX();
+            int mouse_y = GetMouseY();
+            int grid_x = (int) grid.x;
+            int grid_y = (int) grid.y;
+
+            mouse_x = max(mouse_x, grid_x);
+            mouse_x = min(mouse_x, grid_x + grid.width - tile_texture.width);
+            mouse_y = max(mouse_y, grid_y);
+            mouse_y = min(mouse_y, grid_y + grid.height - tile_texture.height);
+
+            mouse_x -= (mouse_x - grid_x) % tile_texture.width;
+            mouse_y -= (mouse_y - grid_y) % tile_texture.height;
+
+            if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+                DrawTexture(mine_texture, mouse_x, mouse_y, WHITE);
+            } else {
+                DrawTexture(tile_hover_texture, mouse_x, mouse_y, WHITE);
             }
         }
         EndDrawing();
