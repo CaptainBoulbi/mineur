226b88500a5b552e17610c6a796e48f5475d36bd
_N3m0 <n3m0o7@hotmail.com>
2024-03-12 10:11:34 +0100

    chiffre, chiffre, chibre, chiffre

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 all:
 	gcc -Wall -Wextra -ggdb main.c -o mineur -lraylib
 
+release:
+	gcc -Wall -Wextra -O3 main.c -o mineur -lraylib
+
 run: all
 	./mineur
 
--- main.c
+++ main.c
@@ -34,17 +34,25 @@ int screen_width = 500;
 int screen_height = 550;
 
 Rectangle menu;
-Vec2i nb_cell = {9, 9}; // 9-9 16-16 16-30
 int grid_len = 0;
 
 Rectangle grid = {0};
 
-Image tile_image_orig = {0};
-Texture tile_texture = {0};
-Image tile_hover_image_orig = {0};
-Texture tile_hover_texture = {0};
-Image mine_image_orig = {0};
-Texture mine_texture = {0};
+#define INIT_TEXTURE(img, tex) \
+    Image img = {0}; \
+    Texture tex = {0}
+
+INIT_TEXTURE(tile_image_orig, tile_texture);
+INIT_TEXTURE(tile_hover_image_orig, tile_hover_texture);
+INIT_TEXTURE(mine_image_orig, mine_texture);
+INIT_TEXTURE(t1_image_orig, t1_texture);
+INIT_TEXTURE(t2_image_orig, t2_texture);
+INIT_TEXTURE(t3_image_orig, t3_texture);
+INIT_TEXTURE(t4_image_orig, t4_texture);
+INIT_TEXTURE(t5_image_orig, t5_texture);
+INIT_TEXTURE(t6_image_orig, t6_texture);
+INIT_TEXTURE(t7_image_orig, t7_texture);
+INIT_TEXTURE(t8_image_orig, t8_texture);
 
 void screen_resize_handle(void)
 {
@@ -59,17 +67,42 @@ void screen_resize_handle(void)
         grid_len, grid_len
     };
 
-    Image tile_copy = ImageCopy(tile_image_orig);
-    ImageResize(&tile_copy, grid.width/nb_cell.x, grid.height/nb_cell.y);
-    tile_texture = LoadTextureFromImage(tile_copy);
+#define RESIZE_TEXTURE(name, copy, orig) \
+    do { \
+        Image copy = ImageCopy(orig); \
+        ImageResize(&(copy), grid.width/game_size.x, grid.height/game_size.y);\
+        name = LoadTextureFromImage(copy); \
+    } while (0)
 
-    Image tile_hover_copy = ImageCopy(tile_hover_image_orig);
-    ImageResize(&tile_hover_copy, grid.width/nb_cell.x, grid.height/nb_cell.y);
-    tile_hover_texture = LoadTextureFromImage(tile_hover_copy);
+    RESIZE_TEXTURE(tile_texture, tile_copy, tile_image_orig);
+    RESIZE_TEXTURE(tile_hover_texture, tile_hover_copy, tile_hover_image_orig);
+    RESIZE_TEXTURE(mine_texture, mine_copy, mine_image_orig);
+    RESIZE_TEXTURE(t1_texture, t1_copy, t1_image_orig);
+    RESIZE_TEXTURE(t2_texture, t2_copy, t2_image_orig);
+    RESIZE_TEXTURE(t3_texture, t3_copy, t3_image_orig);
+    RESIZE_TEXTURE(t4_texture, t4_copy, t4_image_orig);
+    RESIZE_TEXTURE(t5_texture, t5_copy, t5_image_orig);
+    RESIZE_TEXTURE(t6_texture, t6_copy, t6_image_orig);
+    RESIZE_TEXTURE(t7_texture, t7_copy, t7_image_orig);
+    RESIZE_TEXTURE(t8_texture, t8_copy, t8_image_orig);
+}
 
-    Image mine_copy = ImageCopy(mine_image_orig);
-    ImageResize(&mine_copy, grid.width/nb_cell.x, grid.height/nb_cell.y);
-    mine_texture = LoadTextureFromImage(mine_copy);
+int count_bomb(int x, int y)
+{
+    int count = 0;
+    for (int i=-1; i<=1; i++) {
+        if (x+i < 0 || x+i > game_size.x)
+            continue;
+        for (int j=-1; j<=1; j++) {
+            if (y+j < 0 || y+j > game_size.y)
+                continue;
+            if (i == 0 && j == 0)
+                continue;
+            if (game[y+j][x+i] == 'X')
+                count++;
+        }
+    }
+    return count;
 }
 
 void fill_game(GameType type)
@@ -115,24 +148,6 @@ void fill_game(GameType type)
     }
 }
 
-int count_bomb(int x, int y)
-{
-    int count = 0;
-    for (int i=-1; i<=1; i++) {
-        if (x+i < 0 || x+i > game_size.x)
-            continue;
-        for (int j=-1; j<=1; j++) {
-            if (y+j < 0 || y+j > game_size.y)
-                continue;
-            if (i == 0 && j == 0)
-                continue;
-            if (game[y+j][x+i] == 'X')
-                count++;
-        }
-    }
-    return count;
-}
-
 int main(void)
 {
     srand(time(NULL));
@@ -143,6 +158,15 @@ int main(void)
     tile_image_orig = LoadImage("ressources/tile.png");
     tile_hover_image_orig = LoadImage("ressources/tile_hover.png");
     mine_image_orig = LoadImage("ressources/mine.png");
+    mine_image_orig = LoadImage("ressources/mine.png");
+    t1_image_orig = LoadImage("ressources/1.png");
+    t2_image_orig = LoadImage("ressources/2.png");
+    t3_image_orig = LoadImage("ressources/3.png");
+    t4_image_orig = LoadImage("ressources/4.png");
+    t5_image_orig = LoadImage("ressources/5.png");
+    t6_image_orig = LoadImage("ressources/6.png");
+    t7_image_orig = LoadImage("ressources/7.png");
+    t8_image_orig = LoadImage("ressources/8.png");
 
     InitWindow(screen_width, screen_height, "mineur");
     SetTargetFPS(60);
@@ -162,40 +186,43 @@ int main(void)
             if (IsKeyPressed(KEY_R)) {
                 fill_game(BEGINNER);
             }
+            if (IsKeyPressed(KEY_P)) {
+                for (int x=0; x<game_size.x; x++) {
+                    for (int y=0; y<game_size.y; y++) {
+                        printf("%c", game[y][x]);
+                    }
+                    printf("\n");
+                }
+            }
 
             DrawRectangleRec(menu, RED);
             DrawRectangleRec(grid, (Color) {
-                .r = 0xFF, .g = 0xDA, .b = 0x2E, .a = 255
+                .r = 255, .g = 240, .b = 172, .a = 255
             });
 
-            for (int x=0; x<nb_cell.x; x++) {
-                for (int y=0; y<nb_cell.y; y++) {
+            for (int x=0; x<game_size.x; x++) {
+                for (int y=0; y<game_size.y; y++) {
+                    Texture *tex;
                     switch (game[y][x]) {
-                    case '0':
-                        DrawTexture(
-                            tile_texture,
-                            grid.x + grid.width/nb_cell.x * x,
-                            grid.y + grid.height/nb_cell.y * y,
-                            WHITE
-                        );
-                        break;
-                    case 'X':
-                        DrawTexture(
-                            mine_texture,
-                            grid.x + grid.width/nb_cell.x * x,
-                            grid.y + grid.height/nb_cell.y * y,
-                            WHITE
-                        );
-                        break;
                     default:
-                        DrawTexture(
-                            tile_hover_texture,
-                            grid.x + grid.width/nb_cell.x * x,
-                            grid.y + grid.height/nb_cell.y * y,
-                            WHITE
-                        );
-                        break;
+                    case 'X': tex = &mine_texture; break;
+                    case '0': tex = &tile_texture; break;
+                    case '1': tex = &t1_texture; break;
+                    case '2': tex = &t2_texture; break;
+                    case '3': tex = &t3_texture; break;
+                    case '4': tex = &t4_texture; break;
+                    case '5': tex = &t5_texture; break;
+                    case '6': tex = &t6_texture; break;
+                    case '7': tex = &t7_texture; break;
+                    case '8': tex = &t8_texture; break;
                     }
+
+                    DrawTexture(
+                        *tex,
+                        grid.x + grid.width/ game_size.x * x,
+                        grid.y + grid.height/game_size.y * y,
+                        WHITE
+                    );
                 }
             }
 
@@ -206,13 +233,13 @@ int main(void)
 
             // boundary
             mouse_x = max(mouse_x, grid_x);
-            mouse_x = min(mouse_x, grid_x + grid.width - (grid_len/nb_cell.x));
+            mouse_x = min(mouse_x, grid_x + grid.width - (grid_len/game_size.x));
             mouse_y = max(mouse_y, grid_y);
-            mouse_y = min(mouse_y, grid_y + grid.height - (grid_len/nb_cell.y));
+            mouse_y = min(mouse_y, grid_y + grid.height - (grid_len/game_size.y));
 
             // remove extra
-            mouse_x -= (mouse_x - grid_x) % (grid_len/nb_cell.x);
-            mouse_y -= (mouse_y - grid_y) % (grid_len/nb_cell.y);
+            mouse_x -= (mouse_x - grid_x) % (grid_len/game_size.x);
+            mouse_y -= (mouse_y - grid_y) % (grid_len/game_size.y);
 
             if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
                 DrawTexture(mine_texture, mouse_x, mouse_y, WHITE);
